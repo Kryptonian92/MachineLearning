cc698cf9d8d4f87bfa280ddff98b31d45fc8b6fb
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-05-13 23:46:14 -0400

    Final oranges vs apples demo

--- learn.py
+++ learn.py
@@ -1,5 +1,10 @@
 #Simple machine learning to tell an apple from an orange
-import sklearn
+from sklearn import tree
 
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print (clf.predict([[150, 0]]))
+print (clf.predict([[150, 1]]))
+print (clf.predict([[100, 1]]))
